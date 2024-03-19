74efd36bea462ab166ba2631b1300cb6e2afab88
jzalacai <juza989@gmail.com>
2024-03-19 13:02:03 +0100

    feat: Rotten solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,85 @@
 package org.tfoc;
 
-/**
- * The class containing the solution to this exercise
- */
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Solution {
 
-    public Integer search(
-            Integer[] nums,
-            Integer target) {
+    private static final int FRESH_ORANGE = 1;
+    private static final int ROTTEN_ORANGE= 2;
+    private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        return null;
+    public int rotting(int[][] grid) {
+
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        int freshOranges = 0;
+
+        //Saco listado de coordenadas podridas y contador de frescas
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == ROTTEN_ORANGE) {
+                    queue.offer(new int[]{i, j});
+                } else if (grid[i][j] == FRESH_ORANGE) {
+                    freshOranges++;
+                }
+            }
+        }
+
+        int minutes = 0;
+
+        //Si hay naranjas frescas
+        if (freshOranges != 0) {
+            //Mientras queden naranjas podridas sin revisar
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                boolean hasRotten = false;
+
+                for (int i = 0; i < size; i++) {
+                    int[] current = queue.poll();
+                    //Para cada naranja compruebo todas las direcciones posibles
+                    for (int[] dir : DIRECTIONS) {
+                        int x = current[0] + dir[0];
+                        int y = current[1] + dir[1];
+
+                        if (Boolean.TRUE.equals(checkDirection(x, y, rows, cols, grid))) {
+
+                            //Pudro la naranja
+                            grid[x][y] = 2;
+
+                            //Añado la nueva naranja podrida a la cola
+                            queue.offer(new int[]{x, y});
+
+                            //Queda una naranja fresca menos
+                            freshOranges--;
+
+                            //Sigue habiendo naranjas podridas (continuo)
+                            hasRotten = true;
+                        }
+                    }
+                }
+
+                //Una vez analizado las primeras naranjas podridas, si se han pudrido mas, añado un minuto y continuo.
+                if (hasRotten) {
+                    minutes++;
+                }
+            }
+        }
+
+        return freshOranges == 0 ? minutes : -1;
     }
 
+
+    /**
+     * Compruebo si la direccion es posible y si tiene naranja fresca
+     *
+     */
+    private Boolean checkDirection(int x, int y, int rows, int cols, int[][] grid){
+
+        return x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == FRESH_ORANGE;
+    }
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,51 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 class SolutionTest {
 
+
+    private Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Solution();
+    }
+
     @Test
     void testSolution() {
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        int[][] grid1 = {
+                {2, 1, 1},
+                {1, 1, 0},
+                {0, 1, 1}
+        };
+
+        int result = solution.rotting(grid1);
+
+        assertEquals(4, result);
+
+        int[][] grid2 = {
+                {2, 1, 1},
+                {0, 1, 1},
+                {1, 0, 1}
+        };
+
+        result = solution.rotting(grid2);
+
+        assertEquals(-1, result);
+
+        int[][] grid3 = {{0, 2}};
+
+        result = solution.rotting(grid3);
+
+        assertEquals(0, result);
+
     }
 
 }
